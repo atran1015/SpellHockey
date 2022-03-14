2cef11ba799cd7c3b5f9d0f711acf8d112454321
atran1015 <aichantran1015@yahoo.com>
2022-03-14 14:30:06 -0700

    updated PlayerMovement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,20 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D rb;
+
+    public Transform BoundaryHolder;
+
+    Boundary playerBoundary;
+
+    struct Boundary
+    {
+        public float Up, Down, Left, Right;
+
+        public Boundary(float up, float down, float left, float right)
+        {
+            Up = up;
+            Down = down;
+            Left = left;
+            Right = right;
+        }
+    }
+
     void Start()
     {
         //Set Cursor to not be visible
         Cursor.visible = false;
+        rb = GetComponent<Rigidbody2D>();
+
+        playerBoundary = new Boundary(BoundaryHolder.GetChild(0).position.y,
+                                        BoundaryHolder.GetChild(1).position.y,
+                                        BoundaryHolder.GetChild(2).position.x,
+                                        BoundaryHolder.GetChild(3).position.x);
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = mousePosition;
+        //transform.position = mousePosition;
+        //rb.MovePosition(mousePosition);
+
+        Vector2 clampedMousePosition = new Vector2(Mathf.Clamp(mousePosition.x, playerBoundary.Left, playerBoundary.Right), 
+            (Mathf.Clamp(mousePosition.y, playerBoundary.Down, playerBoundary.Up)));
+        rb.MovePosition(clampedMousePosition);
     }
 }
